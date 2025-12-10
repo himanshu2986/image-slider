2b395425acc5b6491ed76cce01068c1dc0de9229
Himanshu Thakur <110151861+himanshu2986@users.noreply.github.com>
2025-12-10 19:14:35 +0530

    Complete

---
+++ script.js
@@ -0,0 +1,89 @@
+const audio = document.getElementById("background-music");
+const playPauseButton = document.getElementById("play-pause-button");
+const playIcon = document.getElementById("play-music");
+const pauseIcon = document.getElementById("pause-music");
+
+let isPlaying = false;
+
+function togglePlayPause() {
+    if (isPlaying) {
+        audio.pause();
+        playIcon.classList.remove("hidden");
+        pauseIcon.classList.add("hidden");
+    } else {
+        audio.play();
+        playIcon.classList.add("hidden");
+        pauseIcon.classList.remove("hidden");
+    }
+    isPlaying = !isPlaying;
+}
+
+playPauseButton.addEventListener("click", togglePlayPause);
+
+// keep looping
+audio.addEventListener("ended", function () {
+    audio.currentTime = 0;
+    audio.play();
+});
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    initialSlide: 4,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    speed: 800,
+    freeMode: false,
+    mousewheel: {
+        thresholdDelta: 30,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    on: {
+        click() {
+            if (typeof swiper.clickedIndex !== "undefined") {
+                swiper.slideTo(swiper.clickedIndex);
+            }
+        },
+    },
+});
+
+// Particles
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 180,
+            density: { enable: true, value_area: 800 },
+        },
+        color: { value: "#fff" },
+        shape: { type: "circle" },
+        opacity: {
+            value: 0.3,
+            anim: { enable: false },
+        },
+        size: {
+            value: 4,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0.1,
+                sync: false,
+            },
+        },
+        line_linked: { enable: false },
+        move: {
+            enable: true,
+            speed: 0.4,
+            direction: "right",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+        },
+    },
+    retina_detect: true,
+});
